bd6619a4c6d48d9b1786ba9a7016ae222ec2e592
Claudia Vitolo <moc0@blanch.ecmwf.int>
2016-12-02 13:23:07 +0000

    Added Mirko's functions

---
+++ R/mean_percs.R
@@ -0,0 +1,30 @@
+#' Calculate average percentile
+#'
+#' @description Calculate average percentile
+#'
+#' @param vals is the a raster object
+#' @param perc.val is the percentile value used as a threshold
+#' @param mod defines if the values considered for the mean are above ('gt') or below ('lt') the threshold
+#'
+
+mean_percs <- function(vals, perc.val, mod){
+  
+  p_val <- stats::quantile(vals, perc.val/100, na.rm = T)
+  
+  if(mod == 'gt'){
+    
+    v_perc <- vals[vals >= p_val]
+    
+  }else if(mod == 'lt'){
+    
+    v_perc <- vals[vals <= p_val]
+    
+  }else{
+    
+    stop("mod should be 'lt' or 'gt'")
+    
+  }
+  
+  return(mean(v_perc, na.rm = T))
+  
+}
---
+++ R/readRisicoBinary.R
@@ -0,0 +1,44 @@
+#' Reads a RISICO output file and returns a raster map
+#'
+#' @description This function reads a RISICO output file and returns a raster map
+#'
+#' @param filename is the file to read
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   r_values <- readRisicoBinary('data/RISICO2015_VPPF_201611140300')
+#' }
+#'
+
+readRisicoBinary <- function(filename){
+  
+	myFile <- gzfile(filename, 'rb')
+	grid_type <- readBin(myFile, integer(), 1)
+
+	if (grid_type == 1){
+	  
+		#regular grid
+		grid_size <- readBin(myFile, 'integer', 2, size = 4)
+		n_values <- grid_size[1]* grid_size[2]
+		
+		lats <- readBin(myFile, "double", 2, size = 4)
+		lons <- readBin(myFile, "double", 2, size = 4)	
+		
+		values_vect <- readBin(myFile, "double", n_values, size = 4)
+		values_vect[values_vect == -9999] <- NA
+		
+		vals <- matrix(values_vect, nrow = grid_size[1], byrow = T)
+		vals <- vals[nrow(vals):1, 1:ncol(vals)]
+		
+		r_values <- raster::raster(vals, xmn = lons[1], xmx = lons[2], 
+		                           ymn = lats[1], ymx = lats[2])
+		
+	}
+	
+	close(myFile)
+
+	return(r_values)
+	
+}
