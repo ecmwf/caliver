015cc6c29d37a21557b66219ca8ef9848736249f
Claudia Vitolo <claudia.vitolo@gmail.com>
2016-12-08 11:54:10 +0000

    Created dummy unit tests for RISICO's functions

--- tests/testthat/test-getPercRiskIndex.R
+++ tests/testthat/test-getPercRiskIndex.R
@@ -3,6 +3,8 @@ context("getPercRiskIndex")
 ## TODO: Rename context
 ## TODO: Add more tests
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("getPercRiskIndex works", {
+  
+  expect_equal(2 * 2, 4) # Change this to something relevant
+  
 })
--- tests/testthat/test-readRisicoBinary.R
+++ tests/testthat/test-readRisicoBinary.R
@@ -3,6 +3,8 @@ context("readRisicoBinary")
 ## TODO: Rename context
 ## TODO: Add more tests
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("readRisicoBinary works", {
+  
+  expect_equal(2 * 2, 4) # Change this to something relevant
+  
 })
