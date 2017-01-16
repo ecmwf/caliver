2678937bee4f83b0dafe569f521fe99be395f80d
Claudia Vitolo <claudia.vitolo@gmail.com>
2017-01-16 12:16:17 +0000

    test for getGFED4basisregions

--- tests/testthat/test-getGFED4BasisRegions.R
+++ tests/testthat/test-getGFED4BasisRegions.R
@@ -2,8 +2,6 @@ context("getGFED4BasisRegions")
 
 test_that("getGFED4BasisRegions works", {
   
-  skip_on_travis()
-  
   GFED4BasisRegions <- getGFED4BasisRegions()
   
   expect_equal(dim(GFED4BasisRegions), c(720, 1440, 1))
