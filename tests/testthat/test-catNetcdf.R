67bf10eee7752a796c286e99cb89d1e0ada15f57
Claudia Vitolo <claudia.vitolo@gmail.com>
2017-04-28 09:37:30 +0100

    brick -> stack in test catNETCDF

--- tests/testthat/test-catNetcdf.R
+++ tests/testthat/test-catNetcdf.R
@@ -15,7 +15,7 @@ test_that("catNetcdf works", {
                           outFile = "TestAB.nc",
                           outDir = myTempDir)
   
-  x <- raster::brick(mergedFile)
+  x <- raster::stack(mergedFile)
   
   expect_equal(dim(x), c(256, 512, 2))
   
