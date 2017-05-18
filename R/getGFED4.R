c638dff8db4f49de1ee36665161abc5c66a1d004
Claudia Vitolo <claudia.vitolo@gmail.com>
2017-05-18 16:03:21 +0100

    Changed check status from x$status == 400 to != 200

--- R/getGFED4.R
+++ R/getGFED4.R
@@ -207,7 +207,7 @@ getGFED4 <- function(startDate = NULL,
                                           overwrite = TRUE)), silent = FALSE)
       
       # Check for unavailable data (HTTP 404 status)
-      if (x$status_code == 404) {
+      if (x$status_code != 200) {
         
         unlink(file.path(myTempDir, inFile))
         message("Either the server or the data are temporarily unavailable.")
