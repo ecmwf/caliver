40fcf2c73684a4cb30ba630199364858bbb82796
Claudia Vitolo <claudia.vitolo@gmail.com>
2017-08-01 10:24:54 +0100

    Integrate linter-check on Travis. This was requested by a reviewer of the PlosOne paper.

--- tests/testthat.R
+++ tests/testthat.R
@@ -5,6 +5,16 @@ library(caliver)
 library(visualTest)
 
 ##  Load the proto library for accessing sub-components of the ggplot2
+library(ggplot2)
 library(proto)
 
 test_check("caliver")
+
+# Static code analysis
+# Integration with lintr: tests to fail if there are any lints in the project
+if (requireNamespace("lintr", quietly = TRUE)) {
+  context("lints")
+  test_that("Package Style", {
+    lintr::expect_lint_free()
+  })
+}
