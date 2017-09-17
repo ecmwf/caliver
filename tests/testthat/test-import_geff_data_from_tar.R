2f876d9be80293720d801428968414cd457474c3
Claudia Vitolo <claudia.vitolo@gmail.com>
2017-09-17 18:33:56 +0100

    Added one more test to import_geff_data_from_tar()

--- tests/testthat/test-import_geff_data_from_tar.R
+++ tests/testthat/test-import_geff_data_from_tar.R
@@ -5,4 +5,10 @@ test_that("import_geff_data_from_tar works", {
   expect_equal("RasterBrick" %in% class(s), TRUE)
   expect_equal(all(dim(s) == c(256, 512, 5)), TRUE)
 
+  geff1tar <- system.file(file.path("testdata", "geff1.tar"),
+                          package = "caliver")
+
+  s2 <- import_geff_data_from_tar(geff1tar, stack_ncfiles = FALSE)
+  expect_equal("character" %in% class(s2), TRUE)
+
 })
