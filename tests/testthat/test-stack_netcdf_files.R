cfeab736a7c9f0e9b5d711f36873226f02bb48cf
Claudia Vitolo <claudia.vitolo@gmail.com>
2017-09-18 10:32:30 +0100

    added test to stack_netcdf_files

--- tests/testthat/test-stack_netcdf_files.R
+++ tests/testthat/test-stack_netcdf_files.R
@@ -2,8 +2,14 @@ context("stack_netcdf_files")
 
 test_that("stack_netcdf_files works", {
 
-  x <- raster::brick(geff5nc)
+  geff0 <- try(stack_netcdf_files(input_dir = NULL,
+                                  pattern = NULL,
+                                  output_file = NULL,
+                                  varname = NULL),
+                silent = TRUE)
+  expect_equal(class(geff0), "try_error")
 
+  x <- raster::brick(geff5nc)
   expect_equal(dim(x), c(256, 512, 5))
 
 })
