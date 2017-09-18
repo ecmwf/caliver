56848fd584ce881db125201fb0e4473b22256f3a
Claudia Vitolo <claudia.vitolo@gmail.com>
2017-09-18 08:16:57 +0100

    Added tests to mean_percs

---
+++ tests/testthat/test-mean_percs.R
@@ -0,0 +1,16 @@
+context("mean_percs")
+
+test_that("mean_percs works", {
+
+  test_data <- readRDS(system.file("extdata",
+                                   "RISICO",
+                                   "RISICO_raster.rds",
+                                   package = "caliver"))
+
+  # Check whether the mod option works correctly
+  percs <- caliver:::mean_percs(vals = test_data, perc_val = 50, mod = "gt")
+  expect_equal(round(percs, 0), 72)
+  percs <- caliver:::mean_percs(vals = test_data, perc_val = 50, mod = "lt")
+  expect_equal(round(percs, 0), 15)
+
+})
