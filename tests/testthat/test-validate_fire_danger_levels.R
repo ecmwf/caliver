9f02b58e7a608ee55c06a5cd26ce750bcf5cb6f0
Claudia Vitolo <claudia.vitolo@gmail.com>
2017-09-18 15:32:08 +0100

    linters

--- tests/testthat/test-validate_fire_danger_levels.R
+++ tests/testthat/test-validate_fire_danger_levels.R
@@ -1,9 +1,9 @@
 context("validate_fire_danger_levels")
 
-x <- raster::raster(ncols=2, nrows=2)
+x <- raster::raster(ncols = 2, nrows = 2)
 x[] <- c(180, 8, 1, 10)
 
-y <- raster::raster(ncols=2, nrows=2)
+y <- raster::raster(ncols = 2, nrows = 2)
 y[] <- c(0, 0.8, 1, 0.3)
 
 test_that("validate_fire_danger_levels works", {
