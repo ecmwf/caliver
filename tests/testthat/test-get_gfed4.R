32f150a4010d268d1b0864f97162eb2c1c454d72
Claudia Vitolo <claudia.vitolo@gmail.com>
2017-09-18 14:12:36 +0100

    Indented commands

--- tests/testthat/test-get_gfed4.R
+++ tests/testthat/test-get_gfed4.R
@@ -58,9 +58,9 @@ test_that("get_gfed4 stops with null varname", {
 test_that("get_gfed4 works with monthly data (1 month)", {
 
   monthly_burned_areas <- get_gfed4(start_date = "2003-01-01",
-                                 end_date = "2003-01-31",
-                                 temporal_resolution = "monthly",
-                                 varname = "BurnedArea")
+                                    end_date = "2003-01-31",
+                                    temporal_resolution = "monthly",
+                                    varname = "BurnedArea")
 
   expect_equal("RasterLayer" %in% class(monthly_burned_areas), TRUE)
   expect_equal(dim(monthly_burned_areas), c(720, 1440, 1))
@@ -70,9 +70,9 @@ test_that("get_gfed4 works with monthly data (1 month)", {
 test_that("get_gfed4 works with daily data (5 days)", {
 
   daily_burned_areas <- get_gfed4(start_date = "2003-01-01",
-                                end_date = "2003-01-05",
-                                temporal_resolution = "daily",
-                                varname = "BurnedArea")
+                                  end_date = "2003-01-05",
+                                  temporal_resolution = "daily",
+                                  varname = "BurnedArea")
 
   expect_equal("RasterBrick" %in% class(daily_burned_areas), TRUE)
   expect_equal(dim(daily_burned_areas), c(720, 1440, 5))
