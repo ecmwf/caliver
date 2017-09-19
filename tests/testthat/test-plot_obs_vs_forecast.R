055684f357b33c01615562c458e5d6a1376ef124
Claudia Vitolo <claudia.vitolo@gmail.com>
2017-09-19 14:57:05 +0100

    linters

--- tests/testthat/test-plot_obs_vs_forecast.R
+++ tests/testthat/test-plot_obs_vs_forecast.R
@@ -8,25 +8,23 @@ test_that("plot_obs_vs_forecast works", {
   x1[] <- runif(400)
   raster::writeRaster(x1, file.path(geffdir,
                                     "20150101_20150101_ecfire_fc_fwi_fwi.nc"),
-                      format="CDF", overwrite = TRUE)
+                      format = "CDF", overwrite = TRUE)
   x2 <- raster::raster(ncols = 20, nrows = 20)
   x2[] <- runif(400)
   raster::writeRaster(x2, file.path(geffdir,
                                     "20150101_20150102_ecfire_fc_fwi_fwi.nc"),
-                      format="CDF", overwrite = TRUE)
+                      format = "CDF", overwrite = TRUE)
   x3 <- raster::raster(ncols = 20, nrows = 20)
   x3[] <- runif(400)
   raster::writeRaster(x3, file.path(geffdir,
                                     "20150102_20150102_ecfire_fc_fwi_fwi.nc"),
-                      format="CDF", overwrite = TRUE)
-  
+                      format = "CDF", overwrite = TRUE)
+
   y <- raster::raster(ncols = 20, nrows = 20)
   y[] <- runif(400)
   y <- raster::stack(y, y)
-  raster::writeRaster(y,
-                      file.path(geffdir,
-                                "CAMS_2015-01-01_2015-01-02_frpfire_rotated.nc"),
-                      format="CDF", overwrite = TRUE)
+  obs_fil <- file.path(geffdir, "CAMS_2015-01-01_2015-01-02_frpfire_rotated.nc")
+  raster::writeRaster(y, obs_fil, format = "CDF", overwrite = TRUE)
 
   polyg <- as(raster::extent(-90, 90, -45, 45), "SpatialPolygons")
   raster::crs(polyg) <- raster::crs(y)
@@ -36,8 +34,7 @@ test_that("plot_obs_vs_forecast works", {
                                   threshold = 0.5,
                                   start_date = "2015-01-01",
                                   end_date = "2015-01-02",
-                                  obs_file_path = file.path(geffdir, 
-                                                            "CAMS_2015-01-01_2015-01-02_frpfire_rotated.nc"),
+                                  obs_file_path = obs_fil,
                                   forecast_type = "fc",
                                   origin = "fwi",
                                   index = "fwi")
