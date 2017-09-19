ae840d179b96f282ad901e81cb545b1eaaa1cf77
Claudia Vitolo <claudia.vitolo@gmail.com>
2017-09-19 15:36:50 +0100

    linters

--- tests/testthat/test-validate_fire_danger_levels.R
+++ tests/testthat/test-validate_fire_danger_levels.R
@@ -7,13 +7,13 @@ y <- raster::raster(ncols = 2, nrows = 2)
 y[] <- c(0, 0.8, 1, 0.3)
 
 test_that("validate_fire_danger_levels works", {
-  
+
   # Check whether the result is correct in case of GLOB
-  tableTest <- validate_fire_danger_levels(fire_index = x,
-                                           observation = y,
-                                           fire_threshold = 10,
-                                           obs_threshold = 0.5)
+  table_test <- validate_fire_danger_levels(fire_index = x,
+                                            observation = y,
+                                            fire_threshold = 10,
+                                            obs_threshold = 0.5)
 
   expect_equal(as.vector(tableTest), c(0, 2, 2, 0))
-  
+
 })
