4137f6b1b2d03c5220fdb7a85033102f596d8043
Claudia Vitolo <claudia.vitolo@gmail.com>
2017-10-09 10:40:08 +0100

    reaRDS IT

--- tests/testthat/test-mask_crop_subset.R
+++ tests/testthat/test-mask_crop_subset.R
@@ -2,9 +2,9 @@ context("mask_crop_subset")
 
 test_that("mask_crop_subset works", {
 
-  p <- raster::raster(system.file(file.path("testdata",
-                                            "GADM_2.8_ITA_adm0.rds"),
-                                  package = "caliver"))
+  p <- readRDS(system.file(file.path("testdata",
+                                     "GADM_2.8_ITA_adm0.rds"),
+                           package = "caliver"))
 
   # Check whether the stopping option works correctly
   x0a <- try(mask_crop_subset("s", p, mask = TRUE, crop = TRUE), silent = TRUE)
