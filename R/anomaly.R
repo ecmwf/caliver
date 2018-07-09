2d4fce7deb9697fd84bf53d3c4fe920a6ac6d516
Claudia Vitolo <claudia.vitolo@gmail.com>
2018-07-09 16:48:41 +0100

    Updated anomaly and ranking + exported related plotting functions

--- R/anomaly.R
+++ R/anomaly.R
@@ -3,23 +3,23 @@
 #' @description This function calculates the anomaly (standardised deviation
 #' from the mean climatology) of a forecast layer
 #'
-#' @param fc is the forecast layer, a Raster* object.
-#' @param clima RasterBrick containing the climatological information
+#' @param r is the Raster layer to compare to the climatology.
+#' @param clima RasterBrick containing the climatological information.
 #'
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#'   fc <- brick("cfwis_ffwi_20170101_1200_00.nc")[[1]]
+#'   r <- brick("cfwis_ffwi_20170101_1200_00.nc")[[1]]
 #'   clima <- brick("fwi.nc")
-#'   x <- anomaly(fc, clima)
+#'   x <- anomaly(r, clima)
 #' }
 #'
 
-anomaly <- function(fc, clima){
+anomaly <- function(r, clima){
 
   # Get the forecast date
-  forecast_date <- substr(x = names(fc), start = 7, stop = nchar(names(fc)))
+  forecast_date <- substr(x = names(r), start = 7, stop = nchar(names(r)))
 
   # Get the climatology dates
   clima_dates <- substr(x = names(clima), start = 7, stop = 11)
@@ -29,28 +29,28 @@ anomaly <- function(fc, clima){
   # Mean from clima
   mean_clima <- raster::calc(x = clima[[clima_idx]],
                             fun = mean, progress = "text")
-  mean_clima <- raster::resample(x = mean_clima, y = fc)
+  mean_clima <- raster::resample(x = mean_clima, y = r)
   # Standard deviation from clima
   sd_clima <- raster::calc(x = clima[[clima_idx]], fun = sd, progress = "text")
-  sd_clima <- raster::resample(x = sd_clima, y = fc)
+  sd_clima <- raster::resample(x = sd_clima, y = r)
 
   # Generate anomaly map
-  anomaly_map <- (fc - mean_clima) / sd_clima
+  anomaly_map <- (r - mean_clima) / sd_clima
 
-  a1 <- fc <= -3
-  a2 <- (fc > -3 & fc <= -2)
-  a3 <- (fc > -2 & fc <= -1.5)
-  a4 <- (fc > -1.5 & fc <= -1)
-  a5 <- (fc > -1 & fc <= -0.5)
-  a6 <- (fc > -0.5 & fc <= +0.5)
-  a7 <- (fc > +0.5 & fc <= +1)
-  a8 <- (fc > +1 & fc <= +1.5)
-  a9 <- (fc > +1.5 & fc <= +2)
-  a10 <- (fc > +2 & fc <= +2.5)
-  a11 <- (fc > +2.5 & fc <= +3)
-  a12 <- fc > 3
+  a1 <- r <= -3
+  a2 <- (r > -3 & r <= -2)
+  a3 <- (r > -2 & r <= -1.5)
+  a4 <- (r > -1.5 & r <= -1)
+  a5 <- (r > -1 & r <= -0.5)
+  a6 <- (r > -0.5 & r <= +0.5)
+  a7 <- (r > +0.5 & r <= +1)
+  a8 <- (r > +1 & r <= +1.5)
+  a9 <- (r > +1.5 & r <= +2)
+  a10 <- (r > +2 & r <= +2.5)
+  a11 <- (r > +2.5 & r <= +3)
+  a12 <- r > 3
 
-  anomaly_map <- fc
+  anomaly_map <- r
   anomaly_map[] <- 0
   anomaly_map[a1] <- 1
   anomaly_map[a2] <- 2
@@ -69,7 +69,24 @@ anomaly <- function(fc, clima){
 
 }
 
-plot_anomaly <- function(r){
+#' @title plot_anomaly
+#'
+#' @description Plot anomaly map as shown in GWIS (\url{https://bit.ly/2BbBfsm})
+#'
+#' @param anomaly_map is the Raster layer, result of \code{anomaly()}.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   r <- brick("cfwis_ffwi_20170101_1200_00.nc")[[1]]
+#'   clima <- brick("fwi.nc")
+#'   anomaly_map <- anomaly(r, clima)
+#'   plot_ranking(anomaly_map)
+#' }
+#'
+
+plot_anomaly <- function(anomaly_map){
 
   breaks <- 1:13
 
@@ -80,14 +97,14 @@ plot_anomaly <- function(r){
 
   # to place the legend outside the map
   par(xpd = FALSE)
-  raster::plot(r, addfun = background_map_fun, col = heatcolors,
+  raster::plot(anomaly_map, addfun = background_map_fun, col = heatcolors,
                breaks = breaks, legend = FALSE)
   par(xpd = TRUE)
-  legend(x = 182, y = 25, legend = c("<=-3.0", "-3.0..-2.0", "-2.0..-1.5",
-                                     "-1.5..-1.0", "-1.0..-0.5",
-                                     "-0.5..0.5", "0.5..1.0", "1.0..1.5",
-                                     "1.5..2.0", "2.0..2.5", "2.5..3.0",
-                                     ">3.0"),
+  legend(x = round(par("usr")[2] + (par("usr")[2] - par("usr")[1])/90, 0),
+         y = round(mean(c(par("usr")[3], par("usr")[4])), 0),
+         legend = c("<=-3.0", "-3.0..-2.0", "-2.0..-1.5", "-1.5..-1.0",
+                    "-1.0..-0.5", "-0.5..0.5", "0.5..1.0", "1.0..1.5",
+                    "1.5..2.0", "2.0..2.5", "2.5..3.0", ">3.0"),
          fill = heatcolors, bty = "n")
 
 }
