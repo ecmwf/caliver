fb6b80ec40894e78243bc1b735d3aecb9867f0fe
Claudia Vitolo <claudia.vitolo@gmail.com>
2018-09-07 13:23:14 +0100

    added vignette and updated boxyplot with newgeff naming convention

--- R/plot_obs_vs_forecast.R
+++ R/plot_obs_vs_forecast.R
@@ -35,7 +35,7 @@ plot_obs_vs_forecast <- function(input_dir,
                                  threshold,
                                  start_date,
                                  end_date,
-                                 obs_file_path,
+                                 obs_file_path =NULL,
                                  forecast_type = "hr",
                                  origin = "cfwis",
                                  index = "ffwi"){
@@ -56,9 +56,9 @@ plot_obs_vs_forecast <- function(input_dir,
       start_d <- gsub("-", "", as.character(my_dates[i]))
 
       file2read <- file.path(input_dir,
-                             paste0(origin, "_", index, "_",
-                                    start_d, "_1200_",
-                                    forecast_type, ".nc"))
+                             paste0("ECMWF_", origin,
+                                    "_", start_d, "_1200_",
+                                    forecast_type, "_", index, ".nc"))
 
       if (file.exists(file2read)) {
         s <- raster::stack(s, raster::brick(file2read))
@@ -126,48 +126,55 @@ plot_obs_vs_forecast <- function(input_dir,
   # reshape the data.frame with forecast values
   x <- reshape2::melt(raster_mean_matrix)
 
-  frp <- raster::brick(obs_file_path)
-  frp_cropped <- mask_crop_subset(r = frp, p = p, mask = TRUE, crop = FALSE)
-  frp_ts <- as.numeric(raster::cellStats(frp_cropped, sum))
-  df_frp <- data.frame(date = forecast_dates[1:length(frp_ts)],
-                      frp_original = frp_ts,
-                      frp = plotrix::rescale(frp_ts, c(0, length(frp_ts))))
-  labels_obs <- round(plotrix::rescale(1:length(frp_ts),
-                                      c(1, max(frp_ts))), 0)[1:length(frp_ts)]
+  if (!is.null(obs_file_path)){
+    frp <- raster::brick(obs_file_path)
+    frp_cropped <- mask_crop_subset(r = frp, p = p, mask = TRUE, crop = FALSE)
+    frp_ts <- as.numeric(raster::cellStats(frp_cropped, sum))
+    df_frp <- data.frame(date = forecast_dates[1:length(frp_ts)],
+                         frp_original = frp_ts,
+                         frp = plotrix::rescale(frp_ts, c(0, length(frp_ts))))
+    labels_obs <- round(plotrix::rescale(1:length(frp_ts),
+                                         c(1, max(frp_ts))), 0)[1:length(frp_ts)]
+  }
 
-  my_name <- "% of pixels\nexceeding the\nhigh danger level"
   # Make the boxy forecast plot using ggplot2
   final_plot <- ggplot(x, aes_string("Var2", "Var1")) +
     geom_tile(aes_string(fill = "value"), colour = "white") +
     scale_fill_distiller(palette = "Spectral",
                          na.value = NA,
                          limits = c(0, 100),
-                         name = my_name) +
-    geom_line(data = df_frp,
-              aes(x = 1:dim(df_frp)[1], y = frp),
-              linetype = 2,
-              col = "#47494c") +
-    scale_color_manual("Fire radiative power",
-                       values = c("gray", "#47494c"),
-                       labels = "Fire radiative power",
-                       guide = guide_legend(direction = "vertical",
-                                            title.position = "top",
-                                            override.aes = list(shape = "A"))) +
+                         name = paste0("% of pixels\nexceeding FWI = ",
+                                       threshold)) +
     theme_bw() + labs(x = "Observation date", y = "Forecast date") +
     scale_x_continuous(expand = c(0, 0),
                        breaks = 1:length(observation_dates),
                        labels = as.character(observation_dates)) +
-    theme(axis.text.x = element_text(angle = 90),
-          panel.grid.major = element_blank(), legend.position = c(.08, .85)) +
     scale_y_continuous(expand = c(0, 0),
                        breaks = 1:length(forecast_dates),
-                       labels = as.character(forecast_dates),
-                       sec.axis = sec_axis(~.,
-                                           name = "Fire radiative power [Wm-2]",
-                                           breaks = 1:length(labels_obs),
-                                           labels = labels_obs)) +
+                       labels = as.character(forecast_dates)) +
+    theme(axis.text.x = element_text(angle = 90),
+          panel.grid.major = element_blank(), legend.position = c(.08, .85)) +
     theme(plot.title = element_text(hjust = 0.5))
+
+  if (!is.null(obs_file_path)){
+    final_plot <- final_plot +
+      geom_line(data = df_frp,
+                aes(x = 1:dim(df_frp)[1], y = frp),
+                linetype = 2,
+                col = "#47494c") +
+      scale_color_manual("Fire radiative power",
+                         values = c("gray", "#47494c"),
+                         labels = "Fire radiative power",
+                         guide = guide_legend(direction = "vertical",
+                                              title.position = "top",
+                                              override.aes = list(shape = "A"))) +
+      scale_y_continuous(sec.axis = sec_axis(~.,
+                                             name = "Fire radiative power [Wm-2]",
+                                             breaks = 1:length(labels_obs),
+                                             labels = labels_obs))
+  }
 
   return(final_plot)
 
 }
+
