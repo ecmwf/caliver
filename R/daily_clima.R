35a274de7b87277ad7ba8aa7611950d5198a08c1
Claudia Vitolo <claudia.vitolo@gmail.com>
2018-10-08 11:09:54 +0100

    fixed lints

--- R/daily_clima.R
+++ R/daily_clima.R
@@ -33,30 +33,31 @@ daily_clima <- function(r, dates, probs){
 
       # which indices correspond to day j?
       idx <- which(substr(names(r), 7, 11) ==
-                     gsub("-", ".", substr(as.character(dates[j]),6,10)))
+                     gsub("-", ".", substr(as.character(dates[j]),6, 10)))
 
-      # Do not take the single day but the period spanning 4 days before and after
-      idxVector <- c()
+      # Do not take the single day but the period spanning 4 days before and
+      # 4 days after the given date
+      idx_vector <- c()
       for (k in seq_along(idx)){
-        idxVector <- c(idxVector, (idx[k] - 4):(idx[k] + 4))
+        idx_vector <- c(idx_vector, (idx[k] - 4):(idx[k] + 4))
       }
-      if (any(idxVector <= 0)){
-        elements2remove <- which(idxVector <= 0)
-        idxVector <- idxVector[-elements2remove]
+      if (any(idx_vector <= 0)){
+        elements2remove <- which(idx_vector <= 0)
+        idx_vector <- idx_vector[-elements2remove]
       }
-      if (any(idxVector > raster::nlayers(r))){
-        elements2remove <- which(idxVector > raster::nlayers(r))
-        idxVector <- idxVector[-elements2remove]
+      if (any(idx_vector > raster::nlayers(r))){
+        elements2remove <- which(idx_vector > raster::nlayers(r))
+        idx_vector <- idx_vector[-elements2remove]
       }
 
-      if(length(idxVector) < length(idx)*9){
+      if(length(idx_vector) < length(idx)*9){
         message(paste0("Caution: climatology for the ", dates[j],
-                       " is calculated using ", length(idxVector),
+                       " is calculated using ", length(idx_vector),
                        " days rather then ", length(idx)*9, "!"))
       }
 
       # Collection of layers spanning the date of interest +/- 4 days & 37 years
-      r_sub <- r[[idxVector]]
+      r_sub <- r[[idx_vector]]
 
       temp_map <- raster::calc(x = r_sub,
                        fun = function(x){raster::quantile(x,
@@ -64,7 +65,7 @@ daily_clima <- function(r, dates, probs){
                                                           na.rm = TRUE)})
       clima_maps[[i]] <- raster::stack(clima_maps[[i]], temp_map)
 
-      rm(j, k, idx, idxVector, r_sub)
+      rm(j, k, idx, idx_vector, r_sub)
 
     }
 
