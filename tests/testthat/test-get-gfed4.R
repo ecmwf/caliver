bef8845d787b566023f29c17a0c0dee5adb64b20
Claudia Vitolo <claudia.vitolo@gmail.com>
2018-11-15 13:49:40 +0000

    fixed lint

--- tests/testthat/test-get-gfed4.R
+++ tests/testthat/test-get-gfed4.R
@@ -3,23 +3,23 @@ context("get_gfed4")
 test_that("Check get_gfed4 behaves as expected", {
 
   # Basis regions: only BONA region
-  BONA <- get_gfed4(varname = "BasisRegions", region = "BONA")
-  expect_true("SpatialPolygonsDataFrame" %in% class(BONA))
-  expect_true(BONA@bbox[[1]] == -178.25)
-  expect_true(BONA@bbox[[2]] == 42)
-  expect_true(BONA@bbox[[3]] == -52.5)
-  expect_true(BONA@bbox[[4]] == 80)
+  bona <- get_gfed4(varname = "BasisRegions", region = "BONA")
+  expect_true("SpatialPolygonsDataFrame" %in% class(bona))
+  expect_true(bona@bbox[[1]] == -178.25)
+  expect_true(bona@bbox[[2]] == 42)
+  expect_true(bona@bbox[[3]] == -52.5)
+  expect_true(bona@bbox[[4]] == 80)
 
   skip("Skip two tests")
   # Monthly burned areas
-  BurnedAreas <- get_gfed4(start_date = "2003-01-01",
-                           end_date = "2003-01-31",
-                           temporal_resolution = "monthly",
-                           varname = "BurnedArea")
+  monthly_burned_areas <- get_gfed4(start_date = "2003-01-01",
+                                    end_date = "2003-01-31",
+                                    temporal_resolution = "monthly",
+                                    varname = "BurnedArea")
   # Daily burned areas
-  DailyBurnedAreas <- get_gfed4(start_date = "2003-01-01",
-                                end_date = "2003-01-02",
-                                temporal_resolution = "daily",
-                                varname = "BurnedArea")
+  daily_burned_areas <- get_gfed4(start_date = "2003-01-01",
+                                  end_date = "2003-01-02",
+                                  temporal_resolution = "daily",
+                                  varname = "BurnedArea")
 
 })
