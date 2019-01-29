4c40e06e0b51a4d754446e7405d4cbe5054bb06b
Claudia Vitolo <claudia.vitolo@gmail.com>
2019-01-29 17:22:42 +0000

    added stack_with_rat()

---
+++ R/stack_with_rat.R
@@ -0,0 +1,43 @@
+#' @title stack_with_rat
+#'
+#' @description This function converts the Raster* object that contains
+#' categorical values to a stack and associates a Raster Attribute Table (rat).
+#' This is useful if plotting using the \code{rasterVis::levelplot()} function.
+#'
+#' @param r is the categorical Raster* object.
+#' @param ids numeric vector containing the categorical levels.
+#' @param classes character vector containing the categorical labels.
+#'
+#' @return The function returns a RasterStack of the same dimensions of
+#' \code{r} but with associated rat.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   x <- stack_with_rat(r, ids = 1:3, classes = c("Low", "Moderate", "High")
+#' }
+#'
+
+stack_with_rat <- function(r,
+                           ids = 1:6,
+                           classes = c("Very low", "Low", "Moderate",
+                                       "High", "Very high", "Extreme")){
+
+  # Define a Raster Attribute Table (RAT)
+  rat <- data.frame(id = ids, danger = classes, stringsAsFactors = FALSE)
+  rat$id <- factor(x = rat$id, levels = ids)
+  rat$danger <- factor(x = rat$danger, levels = classes)
+  names(rat) <- c("ID", "Danger")
+
+  # Transform the brick into a categorical stack of layers
+  index_stack <- raster::stack()
+  for (i in 1:nlayers(r)){
+    tmp <- raster::ratify(r[[i]])
+    levels(tmp) <- rat
+    index_stack <- raster::stack(index_stack, tmp)
+  }
+
+  return(index_stack)
+
+}
