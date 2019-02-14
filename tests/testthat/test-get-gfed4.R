34e4ea2219125790d0ab37e5e9721483fc82ab47
Claudia Vitolo <claudia.vitolo@gmail.com>
2019-02-14 13:52:30 +0000

    Skip on travis tests for get_gfed4

--- tests/testthat/test-get-gfed4.R
+++ tests/testthat/test-get-gfed4.R
@@ -10,11 +10,21 @@ test_that("Check get_gfed4 behaves as expected", {
   expect_true(bona@bbox[[3]] == -52.5)
   expect_true(bona@bbox[[4]] == 80)
 
+  skip_on_travis("Skip")
   # Daily burned areas
   daily_burned_areas <- get_gfed4(start_date = "2003-01-01",
                                   end_date = "2003-01-02",
                                   temporal_resolution = "daily",
                                   varname = "BurnedArea")
   expect_equal(round(max(daily_burned_areas[]), 0), 14384)
+
+  # Monthly burned areas
+  monthly_burned_areas <- get_gfed4(start_date = "2003-01-01",
+                                    end_date = "2003-01-31",
+                                    temporal_resolution = "monthly",
+                                    varname = "BurnedArea")
+  expect_equal(monthly_burned_areas@ncols, 1440)
+  expect_equal(monthly_burned_areas@nrows, 720)
+  expect_equal(round(max(monthly_burned_areas[]), 0), 75219)
 
 })
