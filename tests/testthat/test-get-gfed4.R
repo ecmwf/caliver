1b99d090ee258f49f9630d88abb5af3e1c395604
Claudia Vitolo <claudia.vitolo@gmail.com>
2019-02-14 13:44:20 +0000

    removed monthly test for get_gfed4()

--- tests/testthat/test-get-gfed4.R
+++ tests/testthat/test-get-gfed4.R
@@ -10,15 +10,6 @@ test_that("Check get_gfed4 behaves as expected", {
   expect_true(bona@bbox[[3]] == -52.5)
   expect_true(bona@bbox[[4]] == 80)
 
-  # Monthly burned areas
-  monthly_burned_areas <- get_gfed4(start_date = "2003-01-01",
-                                    end_date = "2003-01-31",
-                                    temporal_resolution = "monthly",
-                                    varname = "BurnedArea")
-  expect_equal(monthly_burned_areas@ncols, 1440)
-  expect_equal(monthly_burned_areas@nrows, 720)
-  expect_equal(round(max(monthly_burned_areas[]), 0), 75219)
-
   # Daily burned areas
   daily_burned_areas <- get_gfed4(start_date = "2003-01-01",
                                   end_date = "2003-01-02",
