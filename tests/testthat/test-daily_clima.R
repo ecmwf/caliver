790471aa76adc805bfec7e85ad43e340743ff477
Claudia Vitolo <claudia.vitolo@gmail.com>
2019-02-23 11:34:27 +0000

    test 1 function at the time

--- tests/testthat/test-daily_clima.R
+++
@@ -1,9 +0,0 @@
-context("daily_clima")
-
-test_that("Testing the file daily_clima.R", {
-
-  x1 <- daily_clima(r = rstack1, dates = "2018.01.01", probs = 0.5)
-  expect_equal(raster::cellStats(x1, min), 2)
-  expect_equal(raster::cellStats(x1, max), 24)
-
-})
--- tests/testthat/test-subset_datacube.R
+++
@@ -1,8 +0,0 @@
-context("subset_datacube")
-
-test_that("Testing the file subset_datacube.R", {
-
-  rstack2 <- subset_datacube(rstack1, "2016-01-01", "2016-01-01")
-  expect_equal(raster::nlayers(rstack2), 1)
-
-})
