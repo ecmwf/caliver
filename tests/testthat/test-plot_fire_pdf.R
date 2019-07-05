1bbcdfb9d39f8be0b2889cf2feb21c911bc14684
Claudia Vitolo <claudia.vitolo@gmail.com>
2019-07-05 15:24:50 +0100

    Remove temporary tests

--- tests/testthat/test-plot_fire_pdf.R
+++ tests/testthat/test-plot_fire_pdf.R
@@ -2,6 +2,7 @@ context("plot_fire_pdf")
 
 test_that("plot_fire_pdf match expectations for no FWI upper_limit", {
 
+  skip("Skip")
   p <- plot_fire_pdf(fire_index = r1,
                      thresholds = c(1, 2, 3, 4, 5),
                      upper_limit = NULL,
@@ -27,6 +28,7 @@ test_that("Plot layers match expectations with FWI upper_limit", {
 
 test_that("Plot layers match expectations with vLines", {
 
+  skip("Skip")
   p <- plot_fire_pdf(fire_index = r1,
                      thresholds = c(1, 2, 3, 4, 5),
                      upper_limit = 15,
