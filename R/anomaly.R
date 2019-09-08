80f0753ea0b65a7c51b8a1401b60e9aad763e867
Claudia Vitolo <claudia.vitolo@gmail.com>
2019-09-08 18:22:12 +0100

    Removed plotting functions for anomaly and ranking as they are equivalent to rasterVis::levelplot()

--- R/anomaly.R
+++ R/anomaly.R
@@ -1,14 +1,16 @@
 #' @title anomaly
 #'
 #' @description This function calculates the anomaly (number of standard
-#' deviations from the mean climatology) of a forecast layer
+#' deviations from the mean climatology) of a forecast layer.
 #'
-#' @param r is the Raster layer to compare to the climatology.
+#' @param r is the RasterLayer to compare to the climatology.
 #' @param clima RasterBrick containing the climatological information.
 #'
-#' @details More information on anomaly is available here:
-#' https://bit.ly/2Qvekz4. You can find fire climatology data for the FWI
-#' indices in Zenodo: https://zenodo.org/communities/wildfire.
+#' @details \code{r} and \code{clima} should have the same extent.
+#' More information on anomaly is available here:
+#' https://bit.ly/2Qvekz4. To estimate fire climatology one can use hindcast or
+#' reanalysis data. Examples of the latter are available from Zenodo:
+#' https://zenodo.org/communities/wildfire.
 #'
 #' @export
 #'
@@ -17,154 +19,61 @@
 #'   r <- brick("cfwis_ffwi_20170101_1200_00.nc")[[1]]
 #'   clima <- brick("fwi.nc")
 #'   x <- anomaly(r, clima)
+#'   
+#'   # This plots nicely using rasterVis::levelplot(), in example on GWIS 
+#'   # (\url{https://gwis.jrc.ec.europa.eu}
+#'   rasterVis::levelplot(x, col.regions = colorRamps::matlab.like(n = 11))
 #' }
 #'
 
 anomaly <- function(r, clima){
 
-  if (round(r@extent@xmin, 0) == 0) {
-
-    # Rotate a Raster* object that has x coordinates (longitude) from 0 to 360,
-    # to standard coordinates between -180 and 180 degrees.
-    # Longitude between 0 and 360 is frequently used in data
-    # from global climate models.
-    r <- raster::rotate(r)
-
+  if (!raster::compareRaster(r, clima)){
+    stop(paste("r and clima are not comparable.",
+               "Please make sure they have same extent, number of rows and",
+               "columns, projection, resolution, and origin."))
   }
 
-  if (round(clima@extent@xmin, 0) == 0) {
+  # Get the forecast dates
+  forecast_date <- substr(x = names(r), start = 2, stop = 11)
 
-    # Rotate a Raster* object that has x coordinates (longitude) from 0 to 360,
-    # to standard coordinates between -180 and 180 degrees.
-    # Longitude between 0 and 360 is frequently used in data
-    # from global climate models.
-    clima <- raster::rotate(clima)
+  # Extract layers corresponding to a given date
+  r_sub <- get_layers_for_clima(clima, forecast_date)
 
-  }
-
-  anomaly_map_cat <- r
-  for (i in 1:raster::nlayers(r)){
-    rx <- r[[i]]
-    # Get the forecast dates
-    forecast_date <- substr(x = names(rx), start = 7, stop = 11)
-
-    # Get the climatology dates
-    clima_dates <- substr(x = names(clima), start = 7, stop = 11)
-    # Get indices of forecast date
-    clima_idx <- which(clima_dates == forecast_date)
-
-    # Mean from clima
-    mean_clima <- raster::calc(x = clima[[clima_idx]], fun = mean)
-    mean_clima <- raster::resample(x = mean_clima, y = rx)
-    # Standard deviation from clima
-    sd_clima <- raster::calc(x = clima[[clima_idx]], fun = sd)
-    sd_clima <- raster::resample(x = sd_clima, y = rx)
-
-    # Generate anomaly map
-    anomaly_map <- (rx - mean_clima) / sd_clima
-
-    # Categorize the anomaly
-    a1 <- anomaly_map <= -3
-    a2 <- (anomaly_map > -3 & anomaly_map <= -2)
-    a3 <- (anomaly_map > -2 & anomaly_map <= -1.5)
-    a4 <- (anomaly_map > -1.5 & anomaly_map <= -1)
-    a5 <- (anomaly_map > -1 & anomaly_map <= -0.5)
-    a6 <- (anomaly_map > -0.5 & anomaly_map <= +0.5)
-    a7 <- (anomaly_map > +0.5 & anomaly_map <= +1)
-    a8 <- (anomaly_map > +1 & anomaly_map <= +1.5)
-    a9 <- (anomaly_map > +1.5 & anomaly_map <= +2)
-    a10 <- (anomaly_map > +2 & anomaly_map <= +3)
-    a11 <- anomaly_map > 3
-
-    if (raster::nlayers(r) > 1){
-      anomaly_map_cat[[i]][] <- NA
-      anomaly_map_cat[[i]][a1] <- 1
-      anomaly_map_cat[[i]][a2] <- 2
-      anomaly_map_cat[[i]][a3] <- 3
-      anomaly_map_cat[[i]][a4] <- 4
-      anomaly_map_cat[[i]][a5] <- 5
-      anomaly_map_cat[[i]][a6] <- 6
-      anomaly_map_cat[[i]][a7] <- 7
-      anomaly_map_cat[[i]][a8] <- 8
-      anomaly_map_cat[[i]][a9] <- 9
-      anomaly_map_cat[[i]][a10] <- 10
-      anomaly_map_cat[[i]][a11] <- 11
-    }else{
-      anomaly_map_cat[] <- NA
-      anomaly_map_cat[a1] <- 1
-      anomaly_map_cat[a2] <- 2
-      anomaly_map_cat[a3] <- 3
-      anomaly_map_cat[a4] <- 4
-      anomaly_map_cat[a5] <- 5
-      anomaly_map_cat[a6] <- 6
-      anomaly_map_cat[a7] <- 7
-      anomaly_map_cat[a8] <- 8
-      anomaly_map_cat[a9] <- 9
-      anomaly_map_cat[a10] <- 10
-      anomaly_map_cat[a11] <- 11
-    }
-  }
+  # Mean from clima
+  mean_clima <- raster::calc(x = r_sub, fun = mean)
+  mean_clima <- raster::resample(x = mean_clima, y = r)
+  # Standard deviation from clima
+  sd_clima <- raster::calc(x = r_sub, fun = sd)
+  sd_clima <- raster::resample(x = sd_clima, y = r)
+  
+  # Generate anomaly map
+  anomaly_map <- (r - mean_clima) / sd_clima
+  
+  # Categorize the anomaly
+  anomaly_map_cat <- raster::cut(anomaly_map,
+                                 breaks = c(-Inf, -3, -2, -1.5, -1, -0.5,
+                                            0.5, 1, 1.5, 2, 3, Inf))
+  
+  # Associate a Raster Attribute Table (RAT)
+  anomaly_map_cat <- raster::ratify(anomaly_map_cat)
+  
+  # Define a Raster Attribute Table (RAT)
+  rat <- data.frame(id = 1:11,
+                    n_stdev = c("<=-3.0", "-3.0..-2.0", "-2.0..-1.5",
+                                "-1.5..-1.0", "-1.0..-0.5", "-0.5..0.5",
+                                "0.5..1.0", "1.0..1.5", "1.5..2.0",
+                                "2.0..3.0", ">3.0"),
+                    stringsAsFactors = FALSE)
+  rat$id <- factor(x = rat$id, levels = 1:11)
+  rat$n_stdev <- factor(x = rat$n_stdev,
+                       levels = c("<=-3.0", "-3.0..-2.0", "-2.0..-1.5",
+                                  "-1.5..-1.0", "-1.0..-0.5", "-0.5..0.5",
+                                  "0.5..1.0", "1.0..1.5", "1.5..2.0",
+                                  "2.0..3.0", ">3.0"))
+  names(rat) <- c("ID", "n_stdev")
+  levels(anomaly_map_cat) <- rat
 
   return(anomaly_map_cat)
-
-}
-
-#' @title plot_anomaly
-#'
-#' @description Plot anomaly map as shown in GWIS (\url{https://bit.ly/2BbBfsm})
-#'
-#' @param anomaly_map is the Raster layer, result of \code{anomaly()}.
-#' @param custom_palette palette to use (default is \code{viridis::plasma})
-#' @param ... other plotting arguments, see \code{?raster::plot} function.
-#'
-#' @export
-#'
-#' @examples
-#' \dontrun{
-#'   r <- brick("cfwis_ffwi_20170101_1200_00.nc")[[1]]
-#'   clima <- brick("fwi.nc")
-#'   anomaly_map <- anomaly(r, clima)
-#'   plot_anomaly(anomaly_map,
-#'                custom_palette = colorRamps::matlab.like(n = length(breaks)))
-#' }
-#'
-
-plot_anomaly <- function(anomaly_map, custom_palette = NULL, ...){
-
-  breaks <- 1:11
-
-  if (is.null(custom_palette)){
-    # Define palette
-    custom_palette <- rev(viridis::plasma(n = length(breaks)))
-  }
-
-  if (raster::nlayers(anomaly_map) == 1){
-    # place the legend outside the map
-    par(xpd = FALSE)
-    raster::plot(anomaly_map,
-                 addfun = .background_map_fun,
-                 col = custom_palette,
-                 breaks = breaks,
-                 legend = FALSE,
-                 ...)
-    par(xpd = TRUE)
-    legend(x = round(par("usr")[2] + (par("usr")[2] - par("usr")[1]) / 90, 0),
-           y = round(mean(c(par("usr")[3], par("usr")[4])), 0),
-           legend = c("<=-3.0", "-3.0..-2.0", "-2.0..-1.5", "-1.5..-1.0",
-                      "-1.0..-0.5", "-0.5..0.5", "0.5..1.0", "1.0..1.5",
-                      "1.5..2.0", "2.0..3.0", ">3.0"),
-           fill = custom_palette, bty = "n")
-  }else{
-    # place the legend beside each map
-    raster::plot(anomaly_map,
-                 addfun = .background_map_fun,
-                 col = custom_palette,
-                 breaks = breaks,
-                 lab.breaks = c("<=-3.0", "-3.0..-2.0", "-2.0..-1.5",
-                                "-1.5..-1.0", "-1.0..-0.5", "-0.5..0.5",
-                                "0.5..1.0", "1.0..1.5", "1.5..2.0", "2.0..3.0",
-                                ">3.0"),
-                 ...)
-  }
 
 }
--- R/ranking.R
+++ R/ranking.R
@@ -4,12 +4,13 @@
 #' percentiles of occurrence of the values based on a given climatology
 #' (see \code{clima}).
 #'
-#' @param r is the Raster layer to compare to the climatology.
+#' @param r is the RasterLayer to compare to the climatology.
 #' @param clima RasterBrick containing the climatological information.
 #'
 #' @details More information on ranking is available here:
-#' https://bit.ly/2Qvekz4. You can find fire climatology data for the FWI
-#' indices in Zenodo: https://zenodo.org/communities/wildfire.
+#' https://bit.ly/2Qvekz4. To estimate fire climatology one can use hindcast or
+#' reanalysis data. Examples of the latter are available from Zenodo:
+#' https://zenodo.org/communities/wildfire.
 #'
 #' @export
 #'
@@ -18,34 +19,32 @@
 #'   r <- brick("cfwis_ffwi_20170101_1200_00.nc")[[1]]
 #'   clima <- brick("fwi.nc")
 #'   x <- ranking(r, clima)
+#'   
+#'   # This plots nicely using rasterVis::levelplot(), in example on GWIS 
+#'   # (\url{https://gwis.jrc.ec.europa.eu}
+#'   rasterVis::levelplot(x, col.regions = colorRamps::matlab.like(n = 6))
+#'   rasterVis::levelplot(x, col.regions = c("green", "yellow", "salmon",
+#'                                           "orange", "red", "black"))
 #' }
 #'
 
 ranking <- function(r, clima){
 
-  if (round(r@extent@xmin, 0) == 0) {
-
-    # Rotate a Raster* object that has x coordinates (longitude) from 0 to 360,
-    # to standard coordinates between -180 and 180 degrees.
-    # Longitude between 0 and 360 is frequently used in data
-    # from global climate models.
-    r <- raster::rotate(r)
-
+  if (!raster::compareRaster(r, clima)){
+    stop(paste("r and clima are not comparable.",
+               "Please make sure they have same extent, number of rows and",
+               "columns, projection, resolution, and origin."))
   }
 
-  if (round(clima@extent@xmin, 0) == 0) {
-
-    # Rotate a Raster* object that has x coordinates (longitude) from 0 to 360,
-    # to standard coordinates between -180 and 180 degrees.
-    # Longitude between 0 and 360 is frequently used in data
-    # from global climate models.
-    clima <- raster::rotate(clima)
-
-  }
-
-  # Set up default layer for comparison
-  prob_maps <- raster::calc(x = clima, fun = .quant_function, progress = "text")
-
+  # Get the forecast dates
+  forecast_date <- substr(x = names(r), start = 2, stop = 11)
+  
+  # Extract layers corresponding to a given date
+  r_sub <- get_layers_for_clima(clima, forecast_date)
+  
+  # Compute reference percentile maps
+  prob_maps <- raster::calc(x = r_sub, fun = .quant_function)
+  # Resample to match forecast layer
   prob_maps <- raster::resample(prob_maps, r, progress = "text")
 
   pbelow75 <- r <= prob_maps[[1]]
@@ -63,66 +62,22 @@ ranking <- function(r, clima){
   ranking_map[p90to95] <- 4
   ranking_map[p95to98] <- 5
   ranking_map[pabove98] <- 6
+  
+  # Associate a Raster Attribute Table (RAT)
+  ranking_map <- raster::ratify(ranking_map)
+  
+  # Define a Raster Attribute Table (RAT)
+  rat <- data.frame(id = 1:6,
+                    category = c("<=75", "75..85", "85..90",
+                                "90..95", "95..98", "98..100"),
+                    stringsAsFactors = FALSE)
+  rat$id <- factor(x = rat$id, levels = 1:6)
+  rat$category <- factor(x = rat$category,
+                        levels = c("<=75", "75..85", "85..90",
+                                   "90..95", "95..98", "98..100"))
+  names(rat) <- c("ID", "category")
+  levels(ranking_map) <- rat
 
   return(ranking_map)
-
-}
-
-#' @title plot_ranking
-#'
-#' @description Plot ranking map as shown in GWIS (\url{https://bit.ly/2BbBfsm})
-#'
-#' @param ranking_map is the Raster layer, result of \code{ranking()}.
-#' @param custom_palette palette to use (default is \code{viridis::viridis})
-#' @param ... other plotting arguments, see \code{?raster::plot} function.
-#'
-#' @export
-#'
-#' @examples
-#' \dontrun{
-#'   r <- brick("cfwis_ffwi_20170101_1200_00.nc")[[1]]
-#'   clima <- brick("fwi.nc")
-#'   ranking_map <- ranking(r, clima)
-#'   # As shown in GWIS
-#'   plot_ranking(ranking_map,
-#'                custom_palette = c("green", "yellow", "salmon", "orange",
-#'                                   "red", "black"))
-#' }
-#'
-
-plot_ranking <- function(ranking_map, custom_palette = NULL, ...){
-
-  breaks <- 1:6
-
-  if (is.null(custom_palette)){
-    # Define palette
-    custom_palette <- rev(viridis::viridis(n = length(breaks)))
-  }
-
-  if (raster::nlayers(ranking_map) == 1){
-    # place the legend outside the map
-    par(xpd = FALSE)
-    raster::plot(ranking_map,
-                 addfun = .background_map_fun,
-                 col = custom_palette,
-                 breaks = breaks,
-                 legend = FALSE,
-                 ...)
-    par(xpd = TRUE)
-    legend(x = round(par("usr")[2] + (par("usr")[2] - par("usr")[1]) / 90, 0),
-           y = round(mean(c(par("usr")[3], par("usr")[4])), 0),
-           legend = c("<=75", "75..85", "85..90",
-                      "90..95", "95..98", "98..100"),
-           fill = custom_palette, bty = "n")
-  }else{
-    # place the legend beside each map
-    raster::plot(ranking_map,
-                 addfun = .background_map_fun,
-                 col = custom_palette,
-                 breaks = breaks,
-                 lab.breaks = c("<=75", "75..85", "85..90",
-                                "90..95", "95..98", "98..100"),
-                 ...)
-  }
 
 }
