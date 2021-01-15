e3ebb9a528b61c1b85c54b4e4b88d233087afdc2
claudia <claudia.vitolo@gmail.com>
2021-01-15 17:06:40 +0000

    Remove unused dependencies

--- R/caliver-package.R
+++ R/caliver-package.R
@@ -9,19 +9,14 @@
 #'
 #' @import ncdf4
 #' @import ggplot2
-#' @importFrom gdalUtils get_subdatasets gdal_translate
 #' @importFrom raster raster stack brick mask crop rotate extent extract t plot
 #' @importFrom raster crs cellFromPolygon cellStats nlayers getData trim overlay
 #' @importFrom raster intersect
 #' @importFrom rworldmap getMap
 #' @importFrom graphics plot legend par
-#' @importFrom stringr str_pad
 #' @importFrom lubridate yday
 #' @importFrom stats median na.omit sd uniroot density quantile
-#' @importFrom RCurl getURL getBinaryURL
 #' @importFrom plotrix rescale
 #' @importFrom reshape2 melt
-#' @importFrom utils download.file untar
-#' @importFrom R.utils gunzip
 #'
 NULL
