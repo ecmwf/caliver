26bef541907bb2f92616ed7aebd9c1dab96b5216
claudia <claudia.vitolo@gmail.com>
2021-02-05 17:34:42 +0000

    Added default values to classify_index

--- R/classify_index.R
+++ R/classify_index.R
@@ -1,12 +1,18 @@
 #' @title classify_index
 #'
 #' @description This function classifies a fire danger index based on user
-#' defined danger thresholds. If no thresholds are specified, the function
-#' assumes the index is FWI and thresholds are those defined by JRC for Europe:
+#' defined danger thresholds. If \code{index} is one of GEFF's indices (fwi,
+#' ffmc, dmc, dc, isi, bui), the user does not need to specify thresholds and
+#' labels. Default thresholds and labels are those defined by JRC for Europe:
+#' https://effis.jrc.ec.europa.eu/about-effis/technical-background/fire-danger-forecast/
+#' For instance if index = "fwi", default thresholds and labels are:
 #' very Low = 0-5.2, low = 5.2-11.2, moderate = 11.2-21.3, high = 21.3-38.0,
 #' very high = 38-50.0, extreme = > 50.
+#' However, if threshold != NULL, the default values will be overwritten by the
+#' custom values.
 #'
 #' @param r is the Raster* object to classify.
+#' @param index this is the code of the fire danger index.
 #' @param thresholds numeric vector containing 5 thresholds.
 #' @param labels string of characters to be used as labels
 #'
@@ -19,11 +25,15 @@
 #'
 #' @examples
 #' \dontrun{
-#'   r <- brick("ECMWF_FWI_20180723_1200_hr_fwi.nc")
-#'   x <- classify_index(r, thresholds = c(5.2, 11.2, 21.3, 38, 50))
+#'   r <- raster::raster("ECMWF_FWI_20180723_1200_hr_fwi.nc")
+#'   x <- classify_index(r, index = "fwi")
 #'
 #'   # This can be plotted using rasterVis::levelplot()
 #'   rasterVis::levelplot(x)
+#'
+#'   # As plot above but using EFFIS pallette
+#'   rasterVis::levelplot(x, col.regions = caliver:::effis_palette)
+#'
 #'   # Plot above plus custom labels
 #'   rasterVis::levelplot(x, names.attr = substring(names(x), 2))
 #'   # Plot above plus custom palette
@@ -37,11 +47,40 @@
 #' }
 #'
 
-classify_index <- function(r, thresholds = NULL, labels = NULL){
+classify_index <- function(r, index = NULL, thresholds = NULL, labels = NULL){
 
-  if (is.null(thresholds) | is.null(labels)){
+  if (index == "fwi" & (is.null(thresholds) | is.null(labels))){
     thresholds <- c(5.2, 11.2, 21.3, 38, 50)
     labels <- c("Very low", "Low", "Moderate", "High", "Very high", "Extreme")
+  }
+  
+  if (index == "ffmc" & (is.null(thresholds) | is.null(labels))){
+    thresholds <- c(82.7, 86.1, 89.2, 93)
+    labels <- c("Very low", "Low", "Moderate", "High", "Very high")
+  }
+  
+  if (index == "dmc" & (is.null(thresholds) | is.null(labels))){
+    thresholds <- c(15.7, 27.9, 53.1, 140.7)
+    labels <- c("Very low", "Low", "Moderate", "High", "Very high")
+  }
+  
+  if (index == "dc" & (is.null(thresholds) | is.null(labels))){
+    thresholds <- c(256.1, 334.1, 450.6, 749.4)
+    labels <- c("Very low", "Low", "Moderate", "High", "Very high")
+  }
+  
+  if (index == "isi" & (is.null(thresholds) | is.null(labels))){
+    thresholds <- c(3.2, 5.0, 7.5, 13.4)
+    labels <- c("Very low", "Low", "Moderate", "High", "Very high")
+  }
+  
+  if (index == "bui" & (is.null(thresholds) | is.null(labels))){
+    thresholds <- c(24.2, 40.7, 73.3, 178.1)
+    labels <- c("Very low", "Low", "Moderate", "High", "Very high")
+  }
+  
+  if (is.null(index) | is.null(thresholds) | is.null(labels)){
+    stop("Please insert correct values for index, thresholds, labels")
   }
 
   index_class <- raster::cut(r, breaks = c(-Inf, thresholds, Inf))
