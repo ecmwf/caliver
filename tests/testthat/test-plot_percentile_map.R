1aa98b697081b4682ad6204b0efd9286ed0e444b
claudia <claudia.vitolo@gmail.com>
2021-02-18 18:48:39 +0000

    removed all but utils

--- tests/testthat/test-plot_percentile_map.R
+++
@@ -1,6 +0,0 @@
-context("test-plot_percentile_map.R")
-
-test_that("plot_percentile_map works", {
-  x <- plot_percentile_map(b)
-  expect_true(is.null(x))
-})
--- tests/testthat/test-subset_datacube.R
+++
@@ -1,8 +0,0 @@
-context("subset_datacube")
-
-test_that("Testing the file subset_datacube.R", {
-
-  rstack2 <- subset_datacube(r = b, from = "1993-01-01", to = "1993-01-01")
-  expect_equal(raster::nlayers(rstack2), 1)
-
-})
